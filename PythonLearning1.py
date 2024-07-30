79a3baab5dcb85c2d5a0509dc29a97bc2a5cbd8b
MaheshG <48827109+gmkumar1988@users.noreply.github.com>
2024-07-29 23:16:27 -0400

    Merge remote-tracking branch 'origin/master'

--- PythonLearning1.py
+++ PythonLearning1.py
@@ -29,11 +29,37 @@ import numpy as np
 #
 # c = (np.linalg.inv(b))
 # print(np.linalg.det(c))
+#
+# m1 = np.array([[2,1],[3,1]])
+# m2 = np.array([[3,5],[1,1]])
+# m3 = np.array([[2,3],[4,5]])
+# determinant_m1_m2_m3 = np.linalg.det(m1*m2*m3)
+# print(determinant_m1_m2_m3)
 
-m1 = np.array([[2,1],[3,1]])
-m2 = np.array([[3,5],[1,1]])
-m3 = np.array([[2,3],[4,5]])
-determinant_m1_m2_m3 = np.linalg.det(m1*m2*m3)
-print(determinant_m1_m2_m3)
+# m = np.array([[1,0,1],[0,1,0],[1,1,1]])
+# n = np.array([[2,8,7],[4,3,9],[1,9,5]])
+# p = np.dot(m,n)
+# print(p)
+# q = np.linalg.det(p)
+# print(q)
+# # np.linalg.det()
 
+# print(np.linalg.det(p))
+# import pandas as pd
+# df = pd.DataFrame({
+# 'A': [1, 2, 2, 3, 4],
+# 'B': [5, 6, 7, 8, 9],
+# 'C': [1, 1, 1, 1, 1]
+# })
+# df['A_rank'] = df['A'].rank()
+# print(df)
 
+a = np.array([[3,5],[2,8]])
+print("Shape of array:\n",np.shape(a))
+print("Covariance matrix of a:\n", np.cov(a))
+# m = np.array([[1,0,1],[0,1,0],[1,1,1]])
+# print("Shape of array:\n",np.shape(m))
+# n = np.array([[2,8,7],[4,3,9],[1,9,5]])
+# print("Shape of array:\n",np.shape(n))
+# print(np.dot(m,n))
+# print(np.linalg.det(np.dot(m,n)))
